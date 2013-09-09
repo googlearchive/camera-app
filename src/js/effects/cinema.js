97d0544dc330ee82669a92ab8c1a9d372e0f3535
Tomasz Mikolajewski <mtomasz@chromium.org>
2013-09-09 16:01:01 +0900

    Added forgotten files.

---
+++ src/js/effects/cinema.js
@@ -0,0 +1,67 @@
+/**
+ * Namespace for the Camera app.
+ */
+var camera = camera || {};
+
+/**
+ * Namespace for effects.
+ */
+camera.effects = camera.effects || {};
+
+/**
+ * @private {camera.effects.Andy} tracker
+ * @constructor
+ * @extend {camera.Effect}
+ */
+camera.effects.Cinema = function(tracker) {
+  camera.Effect.call(this, tracker);
+  this.mode_ = 0;
+};
+
+camera.effects.Cinema.prototype = {
+  __proto__: camera.Effect.prototype
+};
+
+/**
+ * @override
+ */
+camera.effects.Cinema.prototype.randomize = function() {
+  // TODO(mtomasz): To be implemented.
+  this.mode_ = (this.mode_ + 1) % 3;
+};
+
+/**
+ * @override
+ */
+camera.effects.Cinema.prototype.filterFrame = function(canvas) {
+  var face = this.tracker_.getFace();
+  x = canvas.width * (face.x + (face.width / 2));
+  y = canvas.height * face.y * 1.5;
+  radius = Math.sqrt(face.width * face.width +
+                     face.height * face.height) * canvas.width;
+  
+  canvas.tiltShift(0,
+                   y,
+                   canvas.width - 1,
+                   y,
+                   radius / 15.0,
+                   canvas.height * 1.1);
+  canvas.brightnessContrast(0.1, 0.2).
+    vibrance(-1).
+    vignette(0.5, 0.4);
+};
+
+/**
+ * @override
+ */
+camera.effects.Cinema.prototype.getTitle = function() {
+  return chrome.i18n.getMessage('tiltShiftEffect');
+};
+
+/**
+ * @override
+ */
+camera.effects.Cinema.prototype.isSlow = function() {
+  return true;
+};
+
